5c00553ce304f840432617f3aad2b59b02699e74
1128442721 <jerestrepo@cesde.net>
2026-08-04 11:30:30 -0500

    Se ingresa un nuevo cambio com prueba de java con al estacion y src de link

---
+++ java.js
@@ -0,0 +1,11 @@
+
+// Mensajes de prueba
+console.log("Pagina oasis implementando java");
+console.log("Prueba pendiente de validacion");
+
+// Declaración de variables
+let cambiodeversion = "oasis";
+let inicio = "detalle";
+
+console.log("cambio de version :" + cambiodeversion);
+console.log("inicio :" + inicio);
